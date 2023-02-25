da67d02a6743b54e9124414f0beeb86b37e89f04
Ubuntu <giacomo.mugnaini11@gmail.com>
2023-02-25 11:50:20 +0000

    Integration with YOLOv5 tag 6.2

--- man_down_tracking.py
+++ man_down_tracking.py
@@ -1,15 +1,12 @@
-"""
-Man Down Tracking
+# Man Down Tracking 🚀
 
-Run man down tracking algorithm on videos, YouTube, webcam, streams, etc.
+"""
+Run man down tracking algorithm on videos and streams.
 
 Sources:
 
     0, 1, 2, ...                     # webcam
-    img.jpg                          # image
     vid.mp4                          # video
-    path/                            # directory
-    'path/*.jpg'                     # glob
     'https://youtu.be/Zgi9g1ksQHc'   # YouTube
     'rtsp://example.com/media.mp4'   # RTSP, RTMP, HTTP stream
 
@@ -18,6 +15,14 @@ Weights:
     yolov5s.pt                 # PyTorch
     yolov5s.onnx               # ONNX Runtime
     yolov5s.engine             # TensorRT
+
+Models Size:
+
+    'n'   # nano (3.9 MB)
+    's'   # small (14.1 MB)
+    'm'   # medium (40.8 MB)
+    'l'   # large (89.3 MB)
+    'x'   # extra large (166 MB)
 
 """
 
@@ -28,30 +33,32 @@ from pathlib import Path
 import numpy as np
 import cv2
 import torch
+import torch.backends.cudnn as cudnn
 
 FILE = Path(__file__).resolve() # current file path (man_down_tracking.py path)
-ROOT = FILE.parents[0]  # current file root path
+ROOT = FILE.parents[0]  # man down tracking root directory
 WEIGHTS = ROOT / 'weights'  # weights folder path
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 if str(ROOT / 'yolov5') not in sys.path:
-    sys.path.append(str(ROOT / 'yolov5'))  # add yolov5 to PATH
+    sys.path.append(str(ROOT / 'yolov5'))  # add yolov5 directory to PATH
 if str(ROOT / 'deep_sort') not in sys.path:
-    sys.path.append(str(ROOT / 'deep_sort'))  # add deep_sort to PATH
+    sys.path.append(str(ROOT / 'deep_sort'))  # add deep_sort directory to PATH
 if str(ROOT / 'man_down') not in sys.path:
-    sys.path.append(str(ROOT / 'man_down'))  # add man_down to PATH
+    sys.path.append(str(ROOT / 'man_down'))  # add man_down directory to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from yolov5.models.common import DetectMultiBackend
 from deep_sort.deep_sort import DeepSORT
 from man_down.man_down import ManDown
-from tools.general import (Annotator, colors, time_sync, check_img_size, check_file, check_imshow, 
-                            increment_path, select_device, non_max_suppression)
-from tools.load import LoadImages, LoadStreams, IMG_FORMATS, VID_FORMATS, load_yaml
+from tools.general import time_sync, check_img_size, check_file, check_imshow, increment_path, select_device, non_max_suppression
+from tools.load import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams, load_yaml
+from tools.draw import Annotator, colors, color_str
 from tools.save import SaveInfo
 
 # Parameters:
-source = ROOT / 'data/videos/vid1.mp4'  # file/dir/URL/glob/screen/0(webcam)
+source = ROOT / 'data/videos/test1.mp4'  # file/dir/URL/glob/screen/0(webcam)
+source = ROOT / 'data/videos/test1.mp4'
 yolo_weights = WEIGHTS / 'yolov5x.pt'  # YOLOv5 model path
 reid_weights = WEIGHTS / 'osnet_x1_0_market1501.pt'  # ReID model path
 deep_sort_params_path = ROOT / 'data/deep_sort.yaml'  # dataset.yaml path
@@ -76,7 +83,7 @@ visualize = False  # visualize features
 source = str(source)
 is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
 is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
-webcam = source.isnumeric() or source.endswith('.streams') or (is_url and not is_file)
+webcam = source.isnumeric() or source.endswith('.txt') or (is_url and not is_file)
 if is_url and is_file:
     source = check_file(source)  # download
 
@@ -101,22 +108,22 @@ deep_sort = DeepSORT(reid_weights, parameters = deep_sort_params, device = devic
 # Load Logger:
 data_logger = SaveInfo(save_dir, device)
 
-# Dataloader:
-bs = 1  # batch_size
 if webcam:
-    view_img = check_imshow(warn = True)
-    dataset = LoadStreams(source, img_size = imgsz, stride = stride, auto = pt, vid_stride = vid_stride)
-    bs = len(dataset)
+    view_img = check_imshow()
+    cudnn.benchmark = True  # set True to speed up constant image size inference
+    dataset = LoadStreams(source, img_size = imgsz, stride = stride, auto = pt)
+    bs = len(dataset)  # batch_size
 else:
-    dataset = LoadImages(source, img_size = imgsz, stride = stride, auto = pt, vid_stride = vid_stride)
+    dataset = LoadImages(source, img_size = imgsz, stride = stride, auto = pt)
+    bs = 1  # batch_size
 vid_path, vid_writer = [None] * bs, [None] * bs
 
 # Run Algorithm:
-model.warmup(imgsz = (1 if pt or model.triton else bs, 3, *imgsz))  # warmup
-seen, windows, dt = 0, [], [0.0, 0.0, 0.0, 0.0, 0.0]
-t_init = time_sync()
+model.warmup(imgsz = (1 if pt else bs, 3, *imgsz))  # warmup
+N, windows, dt = 0, [], [0.0, 0.0, 0.0, 0.0, 0.0]
+t_start = time_sync()
 for path, img, img0s, vid_cap, s in dataset:
-    # Pre-Process Image:
+    # Pre-process image:
     t1 = time_sync()
     img = np.ndarray.astype(img, dtype = np.half) if half else np.ndarray.astype(img, dtype = np.float32)  # uint8 to fp16/32
     img /= 255  # 0 - 255 to 0.0 - 1.0
@@ -135,14 +142,13 @@ for path, img, img0s, vid_cap, s in dataset:
 
     # Apply NMS:
     t5 = time_sync()
-    pred = pred[0].cpu() if pt else pred.cpu()
+    pred = pred.cpu() if pt else pred.cpu()  # RISISTEMARE
     pred = non_max_suppression(pred, conf_thres = 0.25, iou_thres = 0.45, classes = classes_to_detect, max_det = 1000)
     t6 = time_sync()
     dt[2] = t6 - t5  # post-process speed
 
-    # Process Predictions:
+    # Process predictions:
     for i, det in enumerate(pred):  # per image
-        seen += 1
         if webcam:  # batch_size >= 1
             p, img0, frame = path[i], img0s[i].copy(), dataset.count
             s += f'{i}: '
@@ -151,30 +157,27 @@ for path, img, img0s, vid_cap, s in dataset:
 
         p = Path(p)  # to Path
         save_path = str(save_dir / p.name)  # img.jpg, vid.mp4, ...
-        txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
         s += '%gx%g ' % img.shape[2:]  # print string
         annotator = Annotator(img0, line_width = line_thickness, example = str(names))
 
         if len(det):
-            
-            # Pass Detections to the Man Down Classifier:
+            # Pass detections to the man down classifier:
             t7 = time_sync()
-            md_det = man_down.classify(img, img0, det)
+            md_det = man_down.process_image(img, img0, det)
             t8 = time_sync()
             dt[3] = t8 - t7  # man down classifier speed
 
             if len(md_det):
-
-                # Print results
-                for c in np.unique(md_det[:, 5]):
-                    n = int(np.sum(md_det[:, 5] == c))  # detections per class
+                # Print results:
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 # Pass Detections to the DeepSORT algorithm:
                 t9 = time_sync()
                 outputs = deep_sort.update(img0, md_det)
                 t10 = time_sync()
-                dt[4] = t10 - t9  # deepSORT speed
+                dt[4] = t10 - t9  # DeepSORT speed
 
                 # Draw bounding boxes for visualization:
                 if len(outputs):
@@ -204,19 +207,22 @@ for path, img, img0s, vid_cap, s in dataset:
 
         # Save results (image with detections):
         if save_img:
-            if vid_path != save_path:  # new video
-                vid_path = save_path
-                if isinstance(vid_writer, cv2.VideoWriter):
-                    vid_writer.release()  # release previous video writer
-                if vid_cap:  # video
-                    fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                    w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                    h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                else:  # stream
-                    fps, w, h = 30, img0.shape[1], img0.shape[0]
-
-                vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-            vid_writer.write(img0)
+            if dataset.mode == 'image':
+                cv2.imwrite(save_path, img0)
+            else:
+                if vid_path[i] != save_path:  # new video
+                    vid_path[i] = save_path
+                    if isinstance(vid_writer[i], cv2.VideoWriter):
+                        vid_writer[i].release()  # release previous video writer
+                    if vid_cap:  # video
+                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                    else:  # stream
+                        fps, w, h = 30, img0.shape[1], img0.shape[0]
+                    save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
+                    vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                vid_writer[i].write(img0)
 
     # Save data:
     if save_txt:
@@ -228,15 +234,15 @@ for path, img, img0s, vid_cap, s in dataset:
             GPU_info = data_logger.get_GPU_informations()  # get GPU informations (if available)
             data_logger.save(speed_info, CPU_info, GPU_info)
 
-    print(f"{s}")
+    N += 1
+    print(f'{s}Done. ')
     print(f'speed: {dt[0]*1000:.1f} ms for pre-process, {dt[1]*1000:.1f} ms for inference, {dt[2]*1000:.1f} ms for post-process, {dt[3]*1000:.1f} ms for man down, {dt[4]*1000:.1f} ms for DeepSORT')
 
-t_final = time_sync()
-
-# Compute FPS:
-FPS = seen/(t_final - t_init)
-print("FPS: ", FPS)
+# Print results:
+t_end = time_sync()
+FPS = round(N/(t_end - t_start), 2)  # frame per second
+print(f"FPS: {color_str('bold', FPS)}")
 
 # Save results:
 if save_txt or save_img:
-    print('Results saved to %s' % save_path)
+    print(f"Results saved to {color_str('bold', save_dir)}")
